cc2840dccb7f2e68b70a9c3c5f13a4b0f5cbc210
ghun245 <ghun245@aucklanduni.ac.nz>
2026-07-18 20:27:15 +1200

    major changes, most of event pump done

--- src/board.rs
+++ src/board.rs
@@ -3,13 +3,13 @@ use rand::RngExt;
 // List of neighbour indices, saves time later to declare now
 const NEIGHBOURS: [(isize, isize); 8] = [
     (-1, -1),
-    ( 0, -1),
-    ( 1, -1),
-    (-1,  0),
-    ( 1,  0),
-    (-1,  1),
-    ( 0,  1),
-    ( 1,  1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
 ];
 
 pub struct Cell {
@@ -19,53 +19,48 @@ pub struct Cell {
     pub adjacent: u8,
 }
 pub struct Board {
-    width: usize,
-    height: usize,
-    cells:Vec<Cell>,
-    initialised:bool,
+    pub width: usize,
+    pub height: usize,
+    cells: Vec<Cell>,
+    initialised: bool,
 }
 
-
 impl Board {
-    pub fn new(width: usize, height: usize,num_mines:u8) -> Self {
-        let mut cells = Vec::with_capacity(width*height);
-        for _ in 0..width*height {
+    pub fn new(width: usize, height: usize, num_mines: u8) -> Self {
+        let mut cells = Vec::with_capacity(width * height);
+        for _ in 0..width * height {
             cells.push(Cell {
                 mine: false,
                 revealed: false,
                 flagged: false,
                 adjacent: 0,
-                initialised: false,
             });
         }
-        
-        let mut board = Self{
-            width , 
-            height , 
+
+        let mut board = Self {
+            width,
+            height,
             cells,
+            initialised: false,
         };
         board.set_mines(num_mines);
         board
-        
     }
-// get functions return the cell at (x,y)
-    fn get(&self, x: usize , y: usize) -> &Cell {
-        &self.cells[y*self.width +x]
+    // get functions return the cell at (x,y)
+    fn get(&self, x: usize, y: usize) -> &Cell {
+        &self.cells[y * self.width + x]
     }
-    fn get_mut(&mut self, x :usize, y: usize) -> &mut Cell {
-        &mut self.cells[y*self.width + x]
+    fn get_mut(&mut self, x: usize, y: usize) -> &mut Cell {
+        &mut self.cells[y * self.width + x]
     }
-// counts the number of adjacent mines for pos x,y
-    fn count_neighbours(&self , x: usize , y: usize) -> u8{
-        let mut count = 0u8 ;
+    // counts the number of adjacent mines for pos x,y
+    fn count_neighbours(&self, x: usize, y: usize) -> u8 {
+        let mut count = 0u8;
 
-        for (dx , dy) in NEIGHBOURS {
+        for (dx, dy) in NEIGHBOURS {
             let nx = x as isize + dx;
             let ny = y as isize + dy;
-            if nx >= 0
-                && ny >= 0 
-                && nx < self.width as isize 
-                && ny < self.height as isize {
+            if nx >= 0 && ny >= 0 && nx < self.width as isize && ny < self.height as isize {
                 if self.get(nx as usize, ny as usize).mine {
                     count += 1
                 }
@@ -73,17 +68,17 @@ impl Board {
         }
         count
     }
-// sets the adjacency of each cell 
+    // sets the adjacency of each cell
     pub fn set_adjacent(&mut self) {
         for y in 0..self.height {
             for x in 0..self.width {
-                let count = self.count_neighbours(x,y);
-                self.get_mut(x,y).adjacent = count;
+                let count = self.count_neighbours(x, y);
+                self.get_mut(x, y).adjacent = count;
             }
         }
     }
-// function to place num_mines mines on the board
-    fn set_mines(&mut self,num_mines:u8) {
+    // function to place num_mines mines on the board
+    fn set_mines(&mut self, num_mines: u8) {
         let mut rng = rand::rng();
         let mut placed = 0u8;
         assert!(num_mines as usize <= self.width * self.height);
@@ -91,43 +86,43 @@ impl Board {
         while placed < num_mines {
             let x = rng.random_range(0..self.width);
             let y = rng.random_range(0..self.height);
-            if !self.get(x,y).mine {
-                self.get_mut(x,y).mine = true;
+            if !self.get(x, y).mine {
+                self.get_mut(x, y).mine = true;
                 placed += 1
             }
         }
     }
-// function to guarantee that if the first click on fx,fy is a mine that we safe the cell
-    pub fn ensure_safe_first_click(&mut self, fx: usize , fy: usize) {
-        let mut rng = rand::rng();  
+    // function to guarantee that if the first click on fx,fy is a mine that we safe the cell
+    pub fn ensure_safe_first_click(&mut self, fx: usize, fy: usize) {
+        let mut rng = rand::rng();
 
-        if self.get(fx,fy).mine {
+        if self.get(fx, fy).mine {
             loop {
                 let x = rng.random_range(0..self.width);
                 let y = rng.random_range(0..self.height);
-                if self.get(x,y).mine {
-                    } else {
-                    self.get_mut(x,y).mine = true;
-                    break; 
-                    }
+                if self.get(x, y).mine {
+                } else {
+                    self.get_mut(x, y).mine = true;
+                    break;
+                }
             }
-            self.get_mut(fx,fy).mine = false;
+            self.get_mut(fx, fy).mine = false;
         }
     }
-    pub fn reveal(&mut self , x:usize , y:usize) -> bool {
+// game logic behind revealing a cell
+    pub fn reveal(&mut self, x: usize, y: usize) -> bool {
         if !self.initialised {
-            self.ensure_safe_first_click(x , y);
+            self.ensure_safe_first_click(x, y);
             self.set_adjacent();
             self.initialised = true;
         }
 
-        let cell = self.get_mut(x,y);
 
-        if cell.revealed || cell.flagged {
+        if self.get(x,y).revealed || self.get(x,y).flagged {
             return false;
         }
-        cell.revealed = true;
-        
-        cell.mine 
-    
+        self.get_mut(x,y).revealed = true;
+
+        self.get(x,y).mine
+    }
 }
--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,9 @@
-use crate::board::{Board,Cell};
+const CELL_SIZE: i32 = 32;
+
+const BOARD_X: i32 = 50;
+const BOARD_Y: i32 = 50;
+
+use crate::board::{Board, Cell};
 
 use sdl2::event::Event;
 use sdl2::keyboard::{Keycode, Scancode};
@@ -7,6 +12,7 @@ use sdl2::render::Canvas;
 use sdl2::video::Window;
 
 enum GameState {
+    Menu,
     Running,
     Won,
     Lost,
@@ -19,26 +25,110 @@ pub struct Game {
 
     //game state
     board: Board,
-
-    //State 
-    running: GameState,
+    mines: u8,
+    //State
+    state: GameState,
 }
 
-impl Game{
-    fn new(width:usize, height:usize, num_mines:u8) -> Result<Game,String>{
-        let board = Board::new(width,height,num_mines);
-        let (canvas,event_pump) = sdl_init(width as u32, height as u32)?;
+impl Game {
+    fn new() -> Result<Game, String> {
+        let board = Board::new(9, 9, 10);
+        let (canvas, event_pump) = sdl_init(9, 9)?;
 
         Ok(Game {
             canvas,
             event_pump,
             board,
-            state: GameState::Running,
+            mines: 10,
+            state: GameState::Menu,
         })
     }
-        
+    fn board_to_screen(&self, x: usize, y: usize) -> (i32, i32) {
+        let pixel_x = BOARD_X + x as i32 * CELL_SIZE;
+        let pixel_y = BOARD_Y + y as i32 * CELL_SIZE;
+
+        (pixel_x, pixel_y)
+    }
+    fn screen_to_board(&self, mouse_x: i32, mouse_y: i32) -> Option<(usize, usize)> {
+        let local_x = (mouse_x - BOARD_X) / CELL_SIZE;
+        let local_y = (mouse_y - BOARD_Y) / CELL_SIZE;
+        if local_x < 0
+            || local_y < 0
+            || local_x >= self.board.width as i32 * CELL_SIZE
+            || local_y >= self.board.height as i32 * CELL_SIZE
+        {
+            return None;
+        }
+        let board_x = local_x / CELL_SIZE;
+        let board_y = local_y / CELL_SIZE;
+
+        Some((board_x as usize, board_y as usize))
+    }
+
+    fn handle_input(&mut self) {
+        for event in self.event_pump.poll_iter() {
+            match self.state {
+                GameState::Menu => match event {
+                    Event::KeyDown {
+                        keycode: Some(key), ..
+                    } => match key {
+                        Keycode::Q => {
+                            self.state = GameState::Quit;
+                        }
+
+                        Keycode::Space => {
+                            self.state = GameState::Running;
+                        }
+
+                        Keycode::B => {
+                            self.mines = 10;
+                            self.board = Board::new(9, 9, 10);
+                        }
+
+                        Keycode::I => {
+                            self.mines = 40;
+                            self.board = Board::new(16, 16, 40);
+                        }
+
+                        Keycode::E => {
+                            self.mines = 99;
+                            self.board = Board::new(30, 16, 99);
+                        }
+
+                        _ => {}
+                    },
+
+                    _ => {}
+                },
+
+                GameState::Running => {}
+
+
+
+                _ => match event {
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Space),
+                        ..
+                    } => {
+                        let width = self.board.width;
+                        let height = self.board.height;
+                        self.board = Board::new(width, height, self.mines);
+                        self.state = GameState::Running;
+                    }
+                    Event::KeyDown {
+                        keycode: Some(Keycode::Q),
+                        ..
+                    } => {
+                        self.state = GameState::Menu;
+                    }
+                    _ => {}
+                },
+            }
+        }
+    }
 }
-fn sdl_init(width: u32, height: u32) -> Result<(Canvas<Window>, EventPump), String> {
+
+fn sdl_init(width: u32, height: u32) -> Result<(Canvas<Window>, sdl2::EventPump), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
@@ -47,10 +137,7 @@ fn sdl_init(width: u32, height: u32) -> Result<(Canvas<Window>, EventPump), Stri
         .build()
         .map_err(|e| e.to_string())?;
 
-    let canvas = window
-        .into_canvas()
-        .build()
-        .map_err(|e| e.to_string())?;
+    let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
     let event_pump = sdl_context.event_pump()?;
 
